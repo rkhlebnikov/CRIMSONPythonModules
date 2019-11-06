0771e75479a33c74342a5310bd33760c2b787fcb
Chris Arthurs <carthurs@gmail.com>
2019-11-06 11:09:11 +0000

    Fixed bug with trying to import unwanted modules into the CRIMSON GUI interpreter when the flowsolver package is present.

--- CRIMSONSolver/SolverStudies/__init__.py
+++ CRIMSONSolver/SolverStudies/__init__.py
@@ -1,7 +1,11 @@
 import pkgutil
 
+module_exclude_list = ['flowsolver.plotNetlistCircuits3D',
+					   'flowsolver.plotNetlistOutput']
+
 __all__ = []
 for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    module = loader.find_module(module_name).load_module(module_name)
-    exec('%s = module' % module_name)
+	if module_name not in module_exclude_list:
+	    __all__.append(module_name)
+	    module = loader.find_module(module_name).load_module(module_name)
+	    exec('%s = module' % module_name)
